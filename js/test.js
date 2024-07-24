d82a1da76ea951a9c2a10a93bba9fbf494b4d9a3
GissatSR <D.herfst@gissat.com>
2024-07-24 10:46:12 -0300

    removed test,js

--- js/test.js
+++
@@ -1,9 +0,0 @@
-var webform = new Survey123WebForm({
-    container: "survey_html_element", // this is the element id of the previously create div element
-    itemId: "204359b351b14a838f3bd368960a8d5b" // The attached document explains where to find this
-})
-
-webform.on("formLoaded", async (e) => {
-    let klantnummer = getQuestionValue('field_8');
-    console.log(klantnummer);
-})
