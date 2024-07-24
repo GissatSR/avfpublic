8187026e12fd630d9b9dc2d71b97b34a9d09942f
GissatSR <D.herfst@gissat.com>
2024-07-24 10:42:09 -0300

    Added data parsing

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var webform = new Survey123WebForm({
     container: "survey_html_element", // this is the element id of the previously create div element
     itemId: "204359b351b14a838f3bd368960a8d5b" // The attached document explains where to find this
+    onFormSubmitted: (data) => {
+    console.log('Form submitted: ', data.surveyFeatureSet);
 })
 
 webform.on("formLoaded", async (e) => {
